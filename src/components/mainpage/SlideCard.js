581035f2e1abddb85b637d04ed0013f86f47d701
khazarglzd <101716414+khazarglzd@users.noreply.github.com>
2022-05-19 14:21:22 +0400

    adding Slider and SlideCard comps.

--- src/components/mainpage/SlideCard.js
+++ src/components/mainpage/SlideCard.js
@@ -1,9 +1,41 @@
 import React from "react";
+import Slider from "react-slick";
+import Sdata from "./Sdata";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SlideCard = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    appendDots: (dots) => {
+      return <ul style={{ margin: "0px" }}>{dots}</ul>;
+    },
+  };
   return (
     <>
-      <h1>SlideCard</h1>
+      <Slider {...settings}>
+        {Sdata.map((value, index) => {
+          return (
+            <>
+              <div className="box d_flex top" key={index}>
+                <div className="left">
+                  <h1>{value.title}</h1>
+                  <p>{value.desc}</p>
+                  <button className="btn-primary">Visit Collections</button>
+                </div>
+                <div className="right">
+                  <img src={value.cover} alt="" />
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </Slider>
     </>
   );
 };
